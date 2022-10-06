783ecfe781911bb78f9f7a20ad98bad68ee88f82
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2022-10-07 01:16:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def digital_root(n):
-    sum = 0
-    n = str(n)
-    if len(n)>1:
-        for i in range(len(n)):
-            sum += int(n[i])
-        sum = str(sum)
-    else:
-         sum = int(n)
-    return sum
-    if len(sum) > 1:
-        digital_root(sum)
+def xo(string: str):
+    counter_x = 0
+    counter_o = 0
+    for i in range(len(string)):
+        if string[i]=="x" or string[i]=="X":
+            counter_x+=1
+        elif string[i]=="o" or string[i]=="O":
+            counter_o+=1
+    return counter_x == counter_o
