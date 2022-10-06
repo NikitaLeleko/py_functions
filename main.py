e6737bc6ce4600421a9f31f25e72c25ff0f79dad
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2022-10-07 01:11:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def digital_root(n):
+    sum = 0
+    n = str(n)
+    if len(n)>1:
+        for i in range(len(n)):
+            sum += int(n[i])
+        sum = str(sum)
+    else:
+         sum = int(n)
+    print(sum)
+    if len(sum) > 1:
+        digital_root(sum)
+digital_root(29)
