d8b7e2a71c702f7ce24730f9dd20e7ded4ebeadb
Nikita Leleko <73548163+NikitaLeleko@users.noreply.github.com>
2022-10-11 22:33:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def transposing_matrix(lst: list):
+def multiplication_of_matrix(lst1: list, lst2: list):
     result = []
-    for i in range(len(lst[0])):
-        bufflist = []
-        for j in range(len(lst)):
-            bufflist.append(lst[j][i])
-        result.append(bufflist)
-    return result
+    for i in range(len(lst1)):
+        buff = []
+        for j in range(len(lst1[i])):
+            buff.append(lst1[i][j] * lst2[i][j])
+        result.append(buff)
+    print(result)
